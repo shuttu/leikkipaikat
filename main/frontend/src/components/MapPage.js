db6ec28ea1eda945dd12bba79cc50288a68ab8e8
juhanij <juhjokiko@gmail.com>
2021-05-19 10:47:32 +0300

    Frontend lisätty

---
+++ main/frontend/src/components/MapPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import Button from "react-bootstrap/Button";
+
+function MapPage() {
+    return (
+        <div>
+            <Link to='/'>
+                <Button variant="outline-danger" className="buttonStyle custom-btn">Back</Button>
+            </Link>
+            <h1>Kartta</h1>
+        </div>
+    )
+}
+
+export default MapPage
