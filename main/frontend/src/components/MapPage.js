e019579e917bf1004db7a934eae212d8c4af9323
Lauri Saarenpää <lauri.saarenpaa@learnit.fi>
2021-05-25 17:12:06 +0300

    karttasivun ensimmäinen versio

--- main/frontend/src/components/MapPage.js
+++ main/frontend/src/components/MapPage.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Button from "react-bootstrap/Button";
+import React from "react";
+import { Link } from "react-router-dom";
+import backButton from "../graphics/Back-button.png";
+import infoButton from "../graphics/Info-button.png";
 
 function MapPage() {
-    return (
-        <div>
-            <Link className="link" to='/'>
-                <Button variant="outline-danger" className="buttonStyle custom-btn">Back</Button>
-            </Link>
-            <h1>Kartta</h1>
+  return (
+    <div className="iframe_container">
+      <Link to="/">
+        <div className="backButtonContainer">
+          <img className="backButton" src={backButton} alt="back" />
         </div>
-    )
+      </Link>
+      <div className="infoButtonContainer">
+        <img className="infoButton" src={infoButton} alt="info" />
+      </div>
+      <iframe src="http://127.0.0.1:8000/map/" className="iframe"></iframe>
+    </div>
+  );
 }
 
-export default MapPage
+export default MapPage;
